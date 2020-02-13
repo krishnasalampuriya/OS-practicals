cf1e60bce699dcf1fbc0d117e1e4c07cd492dbde
Krishna Ashok Salampuriya <krishnasalampuriya2000@gmail.com>
2020-02-13 07:01:52 +0000

    Calculate function

--- FCFS.C
+++ FCFS.C
@@ -2,10 +2,12 @@
 int N;   // Number of Processes
 int At[100];  //Arrival timr array
 int Bt[100];   //Burst Time Array
+int Awt,Att;   //Average wating time and average turn around time
 void inputData();
 void calculate();
+/// MAin Function
 int main(){
-    printf(" Hello ");
+    printf("%d %d",Att,Awt);
     return 0;
 }
 
@@ -18,4 +20,17 @@ void inputData(){
         printf("Enter the Burst Time of Process P%d",i);
         scanf("%d",&Bt[i]);
     }
+}
+
+void calculate(){
+    int Ttt=0,Twt=0; //Total turnaround and wating time
+    int Tt=0,Wt=0;   // Turn around Time And Wating Time
+    for(int i=1;i<=N;i++){
+        Wt = Tt - At[i];
+        Tt = Tt + Bt[i];
+        Ttt = Ttt + Tt; 
+        Twt = Wt + Twt;  
+    }
+    Att = Ttt/N;
+    Awt = Twt/N;
 }
